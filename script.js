657c0f13f84d096eaef0c926ffa56f9aec7a38d5
emilainebarbosa-lab <emilaine.barbosa@escola.pr.gov.br>
2026-05-24 15:11:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add JavaScript code for your web site here and call it from index.html.
+function mostrarMensagem() {
+  alert("O agro sustentável é essencial para preservar o planeta e garantir alimentos para o futuro!");
+}
